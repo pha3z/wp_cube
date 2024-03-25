565fee603e7a84870a8667fb588cef30e59abe4f
James <146041997+jhouxatjvx@users.noreply.github.com>
2024-03-25 10:43:54 -0500

    hmmm

---
+++ 99-my-startup.sh
@@ -0,0 +1,27 @@
+#!/command/with-contenv bash
+# https://github.com/just-containers/s6-overlay#container-environment
+# Exit on error
+set -e
+
+cd /var/www/html/public
+
+yes | composer update
+yes | composer install
+
+#you can inject explicit PHP into the config file if necessary. see examples here:
+#https://developer.wordpress.org/cli/commands/config/create/
+if [ ! -f "config.php" ]; then
+	echo "Generating wp-config.php..."
+	wp config create /
+		--dbname=$WP_DB_NAME /
+		--dbuser=$WP_DB_USER /
+		--dbpass=$WP_DB_PASS /
+		--dbhost=$WP_DB_HOST /
+		--locale=en_US-en_US /
+		--allow-root 
+else
+	echo "Existing wp-config.php found. Using it."
+fi
+
+chmod 440 config.php
+
